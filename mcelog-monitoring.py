3c0c0f657aef2b7d25faf44e4734df4a59db9485
ihypnofrog <admin@laptop-2.local>
2013-11-13 22:49:51 +0400

    First version mcelog

---
+++ mcelog-monitoring.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import socket
+import os
+import sys
+import shutil
+
+path = ('/var/log/mcelog')
+second_path = ('/local/tmp/mcelog-count.txt')
+treshold = 100
+
+hostname = socket.gethostname()
+
+if os.path.exists(path) == False:
+        exit()
+
+ExcludeHosts = [ 'zabbix1.mlan', 'zabbix1.msk', 'monitoring4.ulan' ]
+
+if hostname in ExcludeHosts:
+        treshold = 100
+
+step1 = sum(1 for l in open(path, 'r'))
+step2 = sum(1 for l in open(second_path, 'r'))
+step = step1 - step2
+
+if step > treshold:
+        print 1
+else:
+        print 0
+
+shutil.copy(path, secondpath)
