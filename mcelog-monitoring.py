274e8df0d3df30fdc040ecba5090de797bcfeeb2
ihypnofrog <admin@laptop-2.local>
2013-11-16 21:13:03 +0400

    new version script

--- mcelog-monitoring.py
+++ mcelog-monitoring.py
@@ -4,28 +4,52 @@ import socket
 import os
 import sys
 import shutil
+import subprocess
+
+# wwwstaff1.ulan - old server, this is Sun Fire X4100 M2
+
 
 path = ('/var/log/mcelog')
-second_path = ('/local/tmp/mcelog-count.txt')
+second_path = ('/local/tmp/mcelog.log')
 treshold = 100
+hostname = socket.gethostbyaddr(socket.gethostname())[0]
 
-hostname = socket.gethostname()
+if os.path.exists(second_path) == False:
+	subprocess.call(['touch', second_path])
+
+if os.path.exists(path) == False and len(sys.argv) == 2 and  sys.argv[1] == "-d" :
+        print 'Error: file /var/log/mcelog is not found'
+	exit()
 
 if os.path.exists(path) == False:
+        print 0
         exit()
 
-ExcludeHosts = [ 'hostname1', 'hostname2', 'hostname3' ]
-
+ExcludeHosts = [ 'wwwstaff1.ulan', 'host2', 'host3' ] # if you add  new host, please anyway add comment
 if hostname in ExcludeHosts:
-        treshold = 100
+	treshold = 1000
 
-step1 = sum(1 for l in open(path, 'r'))
-step2 = sum(1 for l in open(second_path, 'r'))
-step = step1 - step2
+mcelog_lines_count_path1 = sum(1 for l in open(path, 'r'))
+mcelog_lines_count_second_path = sum(1 for l in open(second_path, 'r'))
+delta = mcelog_lines_count_path1 - mcelog_lines_count_second_path
 
-if step > treshold:
-        print 1
+if delta > treshold:
+	output = 1
+	debug = 'too many errors in mcelog'
 else:
-        print 0
+	output = 0
+	debug = 'all is ok'
 
-shutil.copy(path, secondpath)
+## truncate if mcelog has truncated
+
+if delta < 0:
+	debug = 'delta < 0. truncating file in second_path'
+	target = open(second_path, 'w')
+	target.truncate
+
+if len(sys.argv) == 2 and sys.argv[1] == "-d" :
+        print debug
+else:
+        print output
+
+shutil.copy(path, second_path)
